252c42b50478ac31aa18d42def6a9b8d2bbaa14e
Joseph Eck <jlemxe@gmail.com>
2022-05-24 11:23:32 -0500

    13.5.6

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -8,15 +8,16 @@ console.log("working");
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v4/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let sateliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satalite-streets-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -24,19 +25,20 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  'Day Navigation': light,
-  'Night Navigation': dark
+  'Streets': streets,
+  'Satelite Streets': satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44, -80.0],
-  zoom: 2,
-  layers: [dark]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+
 
 // Accessing the airport GeoJSON URL
 // Having the tileLayer() method before accessing large datasets ensures that the map gets loaded before the data is added to it
@@ -45,19 +47,21 @@ let airportData = "https://raw.githubusercontent.com/JleMxe/Mapping_Earthquakes/
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/JleMxe/Mapping_Earthquakes/main/torontoRoutes.json";
 
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-  console.log(data);
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/JleMxe/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-  // Creating a GeoJSON layer with the retrieved data.
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
   L.geoJSON(data,{
-    color: "#ffffa1",
-    weight: 2,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h2> Airline: " + feature.properties.airline + 
-          "<hr>" + 
-           "Destination: " + feature.properties.dst + 
-           "</h2>");
-    }
+    style: {
+      fillColor: "#ffff00",
+      color: "#0000ff",
+      weight: 1
+    },
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "<hr>");
+        }
+
   }).addTo(map);
 });
